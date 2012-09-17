3b546908a8f9cc41fc641f4f5cc27c3e65226ac1
mmccurdy07 <mmccurdy@gmail.com>
2012-09-16 17:32:51 -0700

    Handle Pandora rate limiting more gracefully, remove Dict saves/loads, tweaks to playlist length options and default.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -19,86 +19,41 @@ def Start():
     DirectoryObject.thumb = R(ICON)
     
     HTTP.CacheTime = 0
-    
-####################################################################################################
-def ValidatePrefs():
-    return
-    
+        
 ####################################################################################################     
 def MainMenu():
+
     oc = ObjectContainer(no_cache=True)
-    
-    if 'PandoraConnection' not in Dict:
-        Dict['PandoraConnection'] = {}
-    
     if not Prefs['pan_user'] or not Prefs['pan_pass']:
         oc.add(PrefsObject(title='Set your Pandora Preferences', thumb=R(ICON_PREFS)))
         return oc
-    elif 'authed' in Dict['PandoraConnection']:
-        if Dict['PandoraConnection']['authed']:
-            authed = Dict['PandoraConnection']['authed']
-        else:
-            authed = Pandora_Authenticate()
-    else :
-        authed = Pandora_Authenticate()
-    
-    if not authed:
-        oc.add(PrefsObject(title='Set your Pandora Preferences', summary='Unable to log in. Please check your settings.', thumb=R(ICON_PREFS)))
-    elif not Dict['PandoraConnection']['premium_account']:
-        oc.add(PrefsObject(title='Enter Pandora Premium membership details', sumamry='The Pandora channel requires a paid subscription to Pandora.', thumb=R(ICON_PREFS)))
     else:
         oc.add(DirectoryObject(key=Callback(StationList), title='Your Stations'))
         oc.add(PrefsObject(title='Change your Pandora Preferences', thumb=R(ICON_PREFS)))
-    return oc
 
-####################################################################################################
-def Pandora_Authenticate():
-    
-    try:
-        pandora = PandoraObject()
-        authed = pandora.authenticated
-    except:
-        pandora = Pandora()
-        authed = pandora.authenticate(Prefs['pan_user'], Prefs['pan_pass'])
-        
-    
-    if authed:
-        Dict['PandoraConnection']['authed'] = True
-        Dict['PandoraConnection']['partner_id'] = pandora.connection.partner_id
-        Dict['PandoraConnection']['partner_auth_token'] = pandora.connection.partner_auth_token
-        Dict['PandoraConnection']['user_id'] = pandora.connection.user_id
-        Dict['PandoraConnection']['user_auth_token'] = pandora.connection.user_auth_token
-        Dict['PandoraConnection']['time_offset'] = pandora.connection.time_offset
-        Dict['PandoraConnection']['premium_account'] = pandora.connection.premium_account
-        return True
-    else:
-        Dict['PandoraConnection']['authed'] = False
-        return False
+    return oc
 
 ####################################################################################################
 def PandoraObject():
     
     pandora = Pandora()
-    
-    pandora.authenticated = Dict['PandoraConnection']['authed']
-    pandora.connection.partner_id = Dict['PandoraConnection']['partner_id']
-    pandora.connection.partner_auth_token = Dict['PandoraConnection']['partner_auth_token']
-    pandora.connection.user_id = Dict['PandoraConnection']['user_id']
-    pandora.connection.user_auth_token = Dict['PandoraConnection']['user_auth_token']
-    pandora.connection.time_offset = Dict['PandoraConnection']['time_offset']
-    pandora.connection.premium_account = Dict['PandoraConnection']['premium_account']
-    
-    return pandora
+    authed = pandora.authenticate(Prefs['pan_user'], Prefs['pan_pass'])
+    if authed:
+        return pandora
+    else:
+        return None
 
 ####################################################################################################
 def StationList():
     
     pandora = PandoraObject()
+    if pandora is None:
+        return ObjectContainer(header="Pandora Error",
+                 message="Unable to log in.  Please check your settings.  The Pandora channel requires a paid Pandora One account."
+               )
     
     oc = ObjectContainer(no_cache=True)
-    
     stations = pandora.get_station_list()
-    
     for station in stations:
         oc.add(DirectoryObject(key=Callback(Station, station=station), title = station['stationName']))
     
@@ -106,28 +61,26 @@ def StationList():
 
 ####################################################################################################
 def Station(station=None, station_id=None):
-    if station:
-        title2 = station['stationName']
-        station_id = station['stationToken']
-    else:
-        title2=station_id
+    
+    title2 = station['stationName']
+    station_id = station['stationToken']
     oc = ObjectContainer(title2=title2, no_cache=True)
     pandora = PandoraObject()
-    pandora.switch_station(station_id)
     
+    try:
+        pandora.switch_station(station_id)
+    except:
+        return ObjectContainer(header="Pandora Error",
+                 message="Unable to switch station.  Hourly limit reached.  Try again in a few minutes."
+               )
+
     while len(oc) < int(Prefs['playlist_length']):
         try:
             song = pandora.get_next_song()
-            try:
-                if song['adToken']:
-                    song = pandora.get_next_song()
-            except:
-                pass
-
             track = GetTrack(song)
             oc.add(track)
-        
         except:
+            Log('Unable to add track.')
             break            
     
     return oc
--- Contents/Code/connection.py
+++ Contents/Code/connection.py
@@ -15,102 +15,55 @@ class PandoraConnection(object):
 	
 	time_offset = 0
 	
-	# Android
-	 
-	# PROTOCOL_VERSION = 'NQ__'
-	# RPC_URL = "Oi8vdHVuZXIucGFuZG9yYS5jb20vc2VydmljZXMvanNvbi8@"
-	# DEVICE_MODEL = "YW5kcm9pZC1nZW5lcmlj"
-	# PARTNER_USERNAME = 'YW5kcm9pZA__'
-	# PARTNER_PASSWORD = 'QUM3SUJHMDlBM0RUU1lNNFI0MVVKV0wwN1ZMTjhKSTc_'
-	# AUDIO_FORMAT_MAP = {'aac': 'HTTP_64_AACPLUS_ADTS',
-	# 					'mp3': 'HTTP_128_MP3'}
+	PROTOCOL_VERSION = 'NQ__'
+	RPC_URL = 'Oi8vaW50ZXJuYWwtdHVuZXIucGFuZG9yYS5jb20vc2VydmljZXMvanNvbi8@'
+	DEVICE_MODEL = 'RDAx'
+	PARTNER_USERNAME = 'cGFuZG9yYSBvbmU_'
+	PARTNER_PASSWORD = 'VFZDS0lCR1M5QU85VFNZTE5ORlVNTDA3NDNMSDgyRA__'
+	ENCRYPT_KEY = 'MiUzV0NMKkpVJE1QXTQ_'
+	DECRYPT_KEY = 'VSNJTyRSWlBBQiVWWDI_'
 
-	# iPhone
-
-	# PROTOCOL_VERSION = String.Encode('5')
-	# RPC_URL = String.Encode('://tuner.pandora.com/services/json/?')
-	# DEVICE_MODEL = String.Encode('IP01')
-	# PARTNER_USERNAME = String.Encode('iphone')
-	# PARTNER_PASSWORD = String.Encode('P2E4FC0EAD3*878N92B2CDp34I0B1@388137C')
-	# ENCRYPT_KEY = String.Encode('721^26xE22776')
-	# DECRYPT_KEY = String.Encode('20zE1E47BE57$51')
-	# AUDIO_FORMAT_MAP = {'aac': 'HTTP_64_AACPLUS_ADTS',
-	# 					'mp3': 'HTTP_128_MP3'}
-	
-	# Palm
-	 
-	# PROTOCOL_VERSION = String.Encode('5')
-	# RPC_URL = String.Encode('://tuner.pandora.com/services/json/?')
-	# DEVICE_MODEL = String.Encode('pre')
-	# PARTNER_USERNAME = String.Encode('palm')
-	# PARTNER_PASSWORD = String.Encode('IUC7IBG09A3JTSYM4N11UJWL07VLH8JP0')
-	# ENCRYPT_KEY = String.Encode('%526CBL$ZU3')
-	# DECRYPT_KEY = String.Encode('E#U$MY$O2B=')
-	# AUDIO_FORMAT_MAP = {'aac': 'HTTP_64_AACPLUS_ADTS',
-	# 					'mp3': 'HTTP_128_MP3'}
-
-	# Pandora One
-
-	PROTOCOL_VERSION = String.Encode('5')
-	RPC_URL = String.Encode('://internal-tuner.pandora.com/services/json/?')
-	DEVICE_MODEL = String.Encode('D01')
-	PARTNER_USERNAME = String.Encode('pandora one')
-	PARTNER_PASSWORD = String.Encode('TVCKIBGS9AO9TSYLNNFUML0743LH82D')
-	ENCRYPT_KEY = String.Encode('2%3WCL*JU$MP]4')
-	DECRYPT_KEY = String.Encode('U#IO$RZPAB%VX2')
-	
 	def __init__(self):
-		self.rid = "%07i" % (Datetime.TimestampFromDatetime(Datetime.Now()) % 1e7)
+		pass
 		
 	def authenticate(self, user, pwd):
-		#try:
+		
 		# partner login
-		partner = self.do_request(
-			'auth.partnerLogin',
-			True,
-			False,
-			deviceModel=String.Decode(self.DEVICE_MODEL),
-			username=String.Decode(self.PARTNER_USERNAME),
-			password=String.Decode(self.PARTNER_PASSWORD),
-			version=String.Decode(self.PROTOCOL_VERSION)
-			)
-		self.partner_id = partner['partnerId']
-		self.partner_auth_token = partner['partnerAuthToken']
+		try:
+			partner = self.do_request(
+				'auth.partnerLogin',
+				True,
+				False,
+				deviceModel=String.Decode(self.DEVICE_MODEL),
+				username=String.Decode(self.PARTNER_USERNAME),
+				password=String.Decode(self.PARTNER_PASSWORD),
+				version=String.Decode(self.PROTOCOL_VERSION)
+				)
+			self.partner_id = partner['partnerId']
+			self.partner_auth_token = partner['partnerAuthToken']
+			
+			# sync
+			pandora_time = int(blowfish.pandora_decrypt(String.Decode(self.DECRYPT_KEY), partner['syncTime'])[4:14])
+			self.time_offset = pandora_time - Datetime.TimestampFromDatetime(Datetime.Now())
+			
+			# user login
+			user = self.do_request('auth.userLogin', True, True, username=user, password=pwd, loginType="user")
+			self.user_id = user['userId']
+			self.user_auth_token = user['userAuthToken']
+			self.premium_account = not user['hasAudioAds']
+		except:
+			pass
 		
-		# sync
-		pandora_time = int(blowfish.pandora_decrypt(String.Decode(self.DECRYPT_KEY), partner['syncTime'])[4:14])
-		self.time_offset = pandora_time - Datetime.TimestampFromDatetime(Datetime.Now())
-		
-		# user login
-		user = self.do_request('auth.userLogin', True, True, username=user, password=pwd, loginType="user")
-		self.user_id = user['userId']
-		self.user_auth_token = user['userAuthToken']
-		self.premium_account = not user['hasAudioAds']
-		
-		return True
-		#except:
-		#	self.partner_id = None
-		#	self.partner_auth_token = None
-		#	self.user_id = None
-		#	self.user_auth_token = None
-		#	self.time_offset = None
-		#	self.premium_account = None
-		#	
-		#	return False
+		if self.partner_auth_token is not None and self.user_auth_token is not None:
+			return True
+		else:
+			return False
 	
 	def get_stations(self):
 		return self.do_request('user.getStationList', False, True)['stations']
 	
-	def get_listener(self):
-		return self.do_request('user.getStationList', False, True)['stations']
-	
 	def get_fragment(self, stationId=None):
-		songlist = self.do_request('station.getPlaylist', True, True, stationToken=stationId)['items']
-				
-		self.curStation = stationId
-		#self.curFormat = format
-		
-		return songlist
+		return self.do_request('station.getPlaylist', True, True, stationToken=stationId)['items']
 	
 	def do_request(self, method, secure, crypted, **kwargs):
 		url_arg_strings = []
@@ -125,41 +78,28 @@ class PandoraConnection(object):
 		
 		url_arg_strings.append('method=%s' % method)
 		url = ('https' if secure else 'http') + String.Decode(self.RPC_URL) + '&'.join(url_arg_strings)
-		
-		# hack used by some other 3rd party clents to get synctime from another source
-		#kwargs['syncTime'] = int(HTTP.Request('http://ridetheclown.com/s2/synctime.php').content)
-		
+				
 		kwargs['syncTime'] = int(Datetime.TimestampFromDatetime(Datetime.Now())+self.time_offset)
 		if self.user_auth_token:
 			kwargs['userAuthToken'] = self.user_auth_token
 		elif self.partner_auth_token:
 			kwargs['partnerAuthToken'] = self.partner_auth_token
 		data = JSON.StringFromObject(kwargs)
-		
 		#Log ('request data --> ' + data)
 		
 		if crypted:
 			data = blowfish.pandora_encrypt(String.Decode(self.ENCRYPT_KEY), data)
 
 		# execute request
-		text = HTTP.Request(url, data=data, headers={'User-agent': "02strich", 'Content-type': 'text/plain'}, cache_time=0).content
-		
-		retry = 0
-		while retry < 3:
-			# parse result
-			tree = JSON.ObjectFromString(text)
-			if tree['stat'] == 'fail':
-				code = tree['code']
-				msg = tree['message']
-				if code == 1002:
-					raise AuthenticationError()
-				elif code == 0:
-					# retry the request
-					text = HTTP.Request(url, data=data, headers={'User-agent': "02strich", 'Content-type': 'text/plain'}, cache_time=0).content
-				else:
-					raise ValueError("%d: %s" % (code, msg))
-			elif 'result' in tree:
-				return tree['result']
+		text = HTTP.Request(url, data=data, headers={'Content-type': 'text/plain'}, cacheTime=0).content
+		#Log ('response data --> ' + text)
+
+		tree = JSON.ObjectFromString(text)
+		if tree['stat'] == 'fail':
+			raise RuntimeError("%d: %s" % (tree['code'], tree['message']))
+			return tree['result']
+		elif 'result' in tree:
+			return tree['result']
 
 
 
